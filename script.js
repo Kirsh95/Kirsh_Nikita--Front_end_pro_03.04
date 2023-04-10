612e35eeac957de947d4921fd74eb834200e8c3e
Fifik66 <kirsh321@gmail.com>
2023-04-10 13:13:47 +0300

    home 5

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = prompt("Введіть кількість годин");
+let time= parseInt(x) * 3600
+
+alert (`${time} секунд в ${x} годинах (-ні) `  )
+
+
+// alert(`
+// ${x} + ${y} = ${ parseInt(x)+parseInt(y)}
+// ${x} - ${y} = ${x - y}
+// ${x} * ${y} = ${x * y}
+// ${x} / ${y} = ${x / y}
+
+// `);
+
