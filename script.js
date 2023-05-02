99e914428c89d11db90ba0fcd0933a202da2b935
Fifik66 <kirsh321@gmail.com>
2023-05-02 20:40:05 +0300

    ДЗ 11. Функція із замиканням

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-
-let array = [1,2,3,4,5,6,7,8,9,10];
-
- console.log(array.length)
-
-let item = 1;
-
-function removeElement(array, item) {
-
-  console.log(array)
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === item) {
-      array.splice(i, 1);
-      i--;
-    }
+function sumClosure() {
+  let result = 0;
+  return function addNum(num) {
+    result += num;
+    return result;
   }
-  return array;
-} 
+}
 
-console.log(removeElement(array, item))
 
-// removeElement()
+const sum = sumClosure();
+
+
+console.log(sum(3)); // Output: 3
+console.log(sum(5)); // Output: 8
+console.log(sum(20)); // Output: 28
+
