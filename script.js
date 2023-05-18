e6e2b920f6219e2afaed5824c9600230c68ef0ef
Fifik66 <kirsh321@gmail.com>
2023-05-18 15:42:01 +0300

    ДЗ 13. this chain

--- script.js
+++ script.js
@@ -1,67 +1,45 @@
 
-let users = [
-    {
-    "index": 0,
-    "isActive": true,
-    "balance": "$2,226.60",
-    "name": "Eugenia Sawyer",
-    "gender": "female",
-    "phone": "+1 (840) 583-3207",
-    "address": "949 John Street, Rose, Puerto Rico, 1857"
+
+
+// let ladder = {
+//     step: 2,
+//     up: function () {
+//         this.step++;
+//     },
+//     down: function () {
+//         this.step--;
+//     },
+//     showStep: function () { // показывает текущую ступеньку
+//         alert(this.step);
+//     }
+// };
+
+
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+
+// ladder.up().up().down().showStep(); // 1
+
+
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
     },
-    {
-    "index": 1,
-    "isActive": true,
-    "balance": "$2,613.77",
-    "name": "Pauline Gallegos",
-    "gender": "female",
-    "phone": "+1 (985) 593-3328",
-    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    down() {
+      this.step--;
+      return this;
     },
-    {
-    "index": 2,
-    "isActive": false,
-    "balance": "$3,976.41",
-    "name": "Middleton Chaney",
-    "gender": "male",
-    "phone": "+1 (995) 591-2478",
-    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-    "index": 3,
-    "isActive": true,
-    "balance": "$1,934.58",
-    "name": "Burns Poole",
-    "gender": "male",
-    "phone": "+1 (885) 559-3422",
-    "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-    "index": 4,
-    "isActive": true,
-    "balance": "$3,261.65",
-    "name": "Mcfadden Horne",
-    "gender": "male",
-    "phone": "+1 (942) 565-3988",
-    "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-    "index": 5,
-    "isActive": false,
-    "balance": "$1,790.56",
-    "name": "Suzette Lewis",
-    "gender": "female",
-    "phone": "+1 (837) 586-3283",
-    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+    showStep() {
+      alert( this.step );
+      return this;
     }
-    ]
-
-
-    let phoneNumbers = users.filter(user => parseFloat(user.balance.replace(',', '').substring(1)) > 2000).map(user => user.phone);
-
-    console.log(phoneNumbers);
-
-
-let totalBalance = users.reduce((acc, user) => acc + parseFloat(user.balance.replace(',', '').substring(1)), 0);
-
-console.log(totalBalance);
+  };
+  
+  ladder.up().up().down().showStep().down().showStep();
