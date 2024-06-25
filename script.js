3b66f01210db9899972ad06c9a025d3c1def03ad
Никита <45015561+Kirsh95@users.noreply.github.com>
2024-06-25 14:59:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,27 @@
+  const years = Number(prompt('вкажіть рік народження'));
+      const city = prompt('вкажіть місто проживання');
+      const sport = prompt('вкажіть улюбленний спорт');  
+      const old = new Date().getFullYear() - years;
+      const capital = {"Київ" : "України", "Вашингтон":"США", "Лондон":"Британії"}
+      const sportCamp = {"Футбол": "Анри", "Формула-1": "Фетел", "Теніс": "Надаль"};
 
-let users = [
-    {
-    "index": 0,
-    "isActive": true,
-    "balance": "$2,226.60",
-    "name": "Eugenia Sawyer",
-    "gender": "female",
-    "phone": "+1 (840) 583-3207",
-    "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-    "index": 1,
-    "isActive": true,
-    "balance": "$2,613.77",
-    "name": "Pauline Gallegos",
-    "gender": "female",
-    "phone": "+1 (985) 593-3328",
-    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-    "index": 2,
-    "isActive": false,
-    "balance": "$3,976.41",
-    "name": "Middleton Chaney",
-    "gender": "male",
-    "phone": "+1 (995) 591-2478",
-    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-    "index": 3,
-    "isActive": true,
-    "balance": "$1,934.58",
-    "name": "Burns Poole",
-    "gender": "male",
-    "phone": "+1 (885) 559-3422",
-    "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-    "index": 4,
-    "isActive": true,
-    "balance": "$3,261.65",
-    "name": "Mcfadden Horne",
-    "gender": "male",
-    "phone": "+1 (942) 565-3988",
-    "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-    "index": 5,
-    "isActive": false,
-    "balance": "$1,790.56",
-    "name": "Suzette Lewis",
-    "gender": "female",
-    "phone": "+1 (837) 586-3283",
-    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-    }
-    ]
+      // 
+      if(years===null || city===null || sport===null){
+        alert(`Шкода що ви відмовились вводити дані`)
+      }
+      else if( (capital.hasOwnProperty(city)) && (sportCamp.hasOwnProperty(sport))){
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city},а це столиця ${capital[city]},ваш улюблений вид спорту ${sport} ти хочешь буду як ${sportCamp[sport]}?`) 
+      }
 
+      else if(!((capital.hasOwnProperty(city))) && ((sportCamp.hasOwnProperty(sport)))){
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city},ваш улюблений вид спорту ${sport} ти хочешь буду як ${sportCamp[sport]}?`) 
+      }
 
-    let phoneNumbers = users.filter(user => parseFloat(user.balance.replace(',', '').substring(1)) > 2000).map(user => user.phone);
+      else if ((capital.hasOwnProperty(city)) && !(sportCamp.hasOwnProperty(sport))){
+          alert(` Ваш вік складає ${old}, живете ви у месті ${city},а це столиця ${capital[city]}, ваш улюблений вид спорту ${sport}`) 
+      }
 
-    console.log(phoneNumbers);
-
-
-let totalBalance = users.reduce((acc, user) => acc + parseFloat(user.balance.replace(',', '').substring(1)), 0);
-
-console.log(totalBalance);
+      else{  !(capital.hasOwnProperty(city)) || !(sportCamp.hasOwnProperty(sport))
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city}, та полюбляете ${sport}`)
+      }
+// 
