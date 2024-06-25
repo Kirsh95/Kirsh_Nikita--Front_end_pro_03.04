2de71b48823405b97e27bcb856a87fde07f9e459
Никита <45015561+Kirsh95@users.noreply.github.com>
2024-06-25 15:03:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,27 @@
-const age = Number(prompt("Введіть рік нардження"));
-const city = prompt("Введіть місто проживання");
-const sport = prompt("Введіть улюблений вид спорту");
+  const years = Number(prompt('вкажіть рік народження'));
+      const city = prompt('вкажіть місто проживання');
+      const sport = prompt('вкажіть улюбленний спорт');  
+      const old = new Date().getFullYear() - years;
+      const capital = {"Київ" : "України", "Вашингтон":"США", "Лондон":"Британії"}
+      const sportCamp = {"Футбол": "Анри", "Формула-1": "Фетел", "Теніс": "Надаль"};
 
-const ageCalc = new Date().getFullYear() - age;
-const capital = [ 'Київ', 'Вашингтон', 'Лондон' ]
-const sportCamp = {
-    'футбол': 'Леонель Месси',
-    'Формула 1': 'Макс Ферстапен',
-    'Баскетбол': 'Майкл Джордан'
-
-};
-
-console.log(ageCalc)
-console.log(city)
-console.log(sport)
-
-
-if (age === null || city === null || sport === null) {
-    alert ( 'Шкода, що Ви не захотіли ввести свій(ю)  ');
-    if (age === null) {
-      alert('рік народження');
-    } else if (city === null) {
-      alert ('назву міста');
-    } else if (sport === null) {
-      alert ('вид спорту');
-    }
-  } 
-  
-  else {
-    alert (`Ваш вік: ${ageCalc}`);
-    // 
-    if (capital.includes(city)) {
-      switch (city) {
-        case 'Київ':
-          country = 'Україна';
-          break;
-        case 'Вашингтон':
-          country = 'США';
-          break;
-        case 'Лондон':
-          country = 'Великобританія';
-          break;
+      // 
+      if(years===null || city===null || sport===null){
+        alert(`Шкода що ви відмовились вводити дані`)
       }
-      alert ( `Ти живеш у столиці ${country}`);
-    } else {
-      alert ( `Ти живеш у місті ${city}`);
-    }
-// 
-    if (sportCamp.hasOwnProperty(sport)) {
-      alert ( `Круто! Ти хочеш стати як ${sportCamp[sport]}?`);
-    }else {
-        alert( `Ти любиш ${sport}`)
-    }
-  }
+      else if( (capital.hasOwnProperty(city)) && (sportCamp.hasOwnProperty(sport))){
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city},а це столиця ${capital[city]},ваш улюблений вид спорту ${sport} ти хочешь буду як ${sportCamp[sport]}?`) 
+      }
 
+      else if(!((capital.hasOwnProperty(city))) && ((sportCamp.hasOwnProperty(sport)))){
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city},ваш улюблений вид спорту ${sport} ти хочешь буду як ${sportCamp[sport]}?`) 
+      }
+
+      else if ((capital.hasOwnProperty(city)) && !(sportCamp.hasOwnProperty(sport))){
+          alert(` Ваш вік складає ${old}, живете ви у месті ${city},а це столиця ${capital[city]}, ваш улюблений вид спорту ${sport}`) 
+      }
+
+      else{  !(capital.hasOwnProperty(city)) || !(sportCamp.hasOwnProperty(sport))
+          alert(`Ваш вік складає ${old}, живете ви у месті ${city}, та полюбляете ${sport}`)
+      }
+// 
